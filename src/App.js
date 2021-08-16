3eb9ab8d313bab52f3a01f4c0612247a21c9efbe
aaronM <ron2805@gmail.com>
2021-08-16 16:39:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Signin from './components/Signin'
+import Dashboard from './components/Dashboard'
+import { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Signin setToken={setToken} />
+  }
+  return (  
+    <div className="wrapper">
+      <h1>Application</h1>
+        <BrowserRouter>
+        <Switch>        
+        <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          {/* <Route path="/preferences">
+            <Preferences />
+          </Route> */}
+        </Switch>
+          </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+// <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
